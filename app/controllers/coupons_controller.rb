6dda3f385e8dd4abe8cfc32e1779fa0cbaf0f034
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-06-10 17:58:14 -0600

    Feat: Merchant Coupon Create and Model Tests

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -6,4 +6,33 @@ class CouponsController < ApplicationController
   def show
     @coupon = Coupon.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = @merchant.coupons.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @new_coupon = @merchant.coupons.new(coupon_params)
+    @new_coupon.status = "deactivated"
+    if @merchant.coupon_limit_5 == true && @new_coupon.save
+      @new_coupon.status = "activated"
+      redirect_to "/merchants/#{@merchant.id}/coupons"
+      flash[:notice] = "Activated Coupon Successfully Created"
+    elsif
+      @new_coupon.save
+      @new_coupon.status = "deactivated"
+      redirect_to "/merchants/#{@merchant.id}/coupons"
+      flash[:notice] = "Deactivated Coupon Successfully Created"
+    else
+      redirect_to "/merchants/#{@merchant.id}/coupons/new"
+      flash[:alert] = "ERROR - VALID DATA MUST BE ENTERED FOR COUPON CREATION"
+    end
+  end
+
+  private
+  def coupon_params
+    params.permit(:name, :code, :amount, :disc_type)
+  end
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,8 +1,12 @@
 class Coupon < ApplicationRecord
-  validates_presence_of :name,
-                        :status,
-                        :code
+  validates :name, presence: true
+  validates :status, presence: true
+  validates :code, presence: true
+  validates :amount, presence: true, numericality: true
+  validates :disc_type, presence: true
+
   belongs_to :merchant
   has_many :invoices
   enum status: [:deactivated, :activated]
+  enum disc_type: [:dollar, :percent]
 end
